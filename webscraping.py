7af40c328eb3c211027c7a3c6d67d9a83cc2a674
Patryk Tokarski <patryk.tokarski@outlook.com>
2021-06-11 09:10:42 +0200

    added miniatures for articles and cleaning code

--- webscraping.py
+++ webscraping.py
@@ -2,9 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import os
 import time
-
 # from requests_html import HTMLSession
-
 import pandas as pd
 
 
@@ -12,17 +10,16 @@ def export_to_html(df):
     html: str = df.to_html(index=0, sparsify=0, render_links=1, escape=0, classes=['table', 'table-striped', 'table-dark'])
     css = """
         <head>
+        <title>Shorter</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
             integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <style>
             a, a:hover, a:visited{
                 color: #fff;
-            }
-            .title {
-                font-size:16px;
-                font-weight:normal;
-                color: black;
-            }            
+            }   
+            h2 {
+                font-weight:bold;
+            }       
         </style>
         </head>
         """
@@ -44,42 +41,47 @@ def export_to_html(df):
 
 
 def add_readable_html(link):
-    print('get')
     source = requests.get(f'https://{link}').text
     soup = BeautifulSoup(source, 'lxml')
-    print('got')
-    if soup:
+    if not soup == '':
         return soup
-    # TODO rendering purepc
     # else:
-    #     session = HTMLSession()
-    #     r = session.get(f'https://{link}')
-    #     r.html.render()
-    #     print(r.html)
-    #     return r.html
+        # TODO rendering purepc
+        # session = HTMLSession()
+        # r = session.get(f'https://{link}')
+        # r.html.render()
+        #
+        # print(f'r.html{r.html}')
+        # return r.html
 
 
 def write_header_to_dict(dictionary, site_address):
-    dictionary['Title'].append(f'<h2>{site_address}</h2>')
-    dictionary['Desc'].append(f'<h2>{site_address}</h2>')
+    dictionary['Title'].append(f'<h2><a href=https://{site_address} >{site_address}<a></h2>')
+    dictionary['Desc'].append(f'<h2><a href=https://{site_address} >{site_address}<a></h2>')
 
 
 class Article:
-    def __init__(self, path):
+    def __init__(self, path, extension=''):
         print(f'Start: {path}')
         self.path = path
+        self.img = ''
+        self.extension = extension
         self.items = {'Title': [], 'Desc': []}
-        self.soup = add_readable_html(path)
+        self.soup = add_readable_html(path+extension)
 
-        write_header_to_dict(self.items, self.path)
+        write_header_to_dict(self.items, path+extension)
 
-    def add_article(self, title, link, desc):
+    def add_article(self, title, link, desc, img=''):
         link = f'https://{self.path}{link}'
-        self.items['Title'].append(f'<a href={link} target="_blank" ><h5>{title}<h5></a>')
+        if img != '': self.add_img(img)
+        self.items['Title'].append(f'<a href={link} target="_blank" ><h5>{title} {self.img}<h5></a>')
         self.items['Desc'].append(desc)
 
     def get_dataframe(self):
         return pd.DataFrame(self.items)
+
+    def add_img(self, img):
+        self.img = f'<br><br><img src={img} width=350px>'
 
 
 def newonce():
@@ -90,13 +92,15 @@ def newonce():
         title = article.find('img')['alt']
         link = article.find('a')['href']
         desc = ''
-        site.add_article(title, link, desc)
+        img = article.find('img')['src']
+
+        site.add_article(title, link, desc, img)
 
     return site.get_dataframe()
 
 
 def gry_online():
-    site = Article('gry-online.pl/newsroom/news/')
+    site = Article('gry-online.pl', '/newsroom/news/')
 
     articles = site.soup.find_all('div', class_='box')
 
@@ -104,6 +108,7 @@ def gry_online():
         title = article.find('h5').text
         link = article.find('a')['href']
         desc = article.find('p', class_='').text
+
         site.add_article(title, link, desc)
 
     return site.get_dataframe()
@@ -111,32 +116,29 @@ def gry_online():
 
 def purepc():
     site = Article('purepc.pl')
-    result = site.soup.find('div', class_='container')
-    articles = result.find_all('div', class_='ln_item')
+    print(site.soup.text)
+    result = site.soup.find_all('div', class_='latest_items')
 
-    for article in articles:
-        article_title = article.find_all('a')[1]['title']
-        article_link = article.find_all('a')[1]['link']
-        article_desc = article.find('p').text
+    print(f'result:{result}')
 
-        site.add_article(article_title, article_link, article_desc)
+    for article in result:
+        articles = article.find('section', class_='ln_item')
+        print(articles)
+        for mini_article in articles:
+            article_title = mini_article.find_all('a')[1]['title']
+            print(article_title)
+            article_link = mini_article.find_all('a')[1]['link']
+            article_desc = mini_article.find('p').text
+
+            site.add_article(article_title, article_link, article_desc)
 
     return site.get_dataframe()
 
 
 def donald():
     def news():
-        path = 'donald.pl/news'
-        site = Article(path)
+        site = Article('donald.pl', '/news')
 
-        # for big first
-        articles = site.soup.find('div', class_='sc-1ucdnva-0')
-        title = articles.find('h3').text
-        link = articles.find('a')['href']
-        desc = ''
-        site.add_article(title, link, desc)
-
-        # for the rest
         articles = site.soup.find_all('div', class_='sc-1uxgugq-5')
         for article in articles:
             title = article.find('a').text
@@ -148,13 +150,39 @@ def donald():
 
     def main_site():
         site = Article('donald.pl')
-        articles = site.soup.find_all('div', class_='sc-1sp7ghq-0')
 
+        # top 1
+        top_articles = site.soup.find('div', class_='sc-23vgyc-1 bFvgnQ')
+
+        article = top_articles.find('div', 'sc-23vgyc-3 fDSkTO')
+
+        title = article.find('h2').text
+        link = article.find('a')['href']
+        desc = ''
+        img = article.find('img')['data-src']
+
+        site.add_article(title, link, desc, img)
+
+        # 2 on the side
+        articles = top_articles.find('div', class_='sc-23vgyc-4 ihtiRV')
+        articles = articles.find_all('li', class_='sc-1uxgugq-0 hiiSmU')
+        for article in articles:
+            title = article.find('h3').text
+            link = article.find('a')['href']
+            desc = ''
+            img = article.find('img')['data-src']
+
+            site.add_article(title, link, desc, img)
+
+        # rest
+        articles = site.soup.find_all('div', class_='sc-1sp7ghq-0')
         for article in articles:
             title = article.find('div', class_='hys1q5-0').text
             link = article.find('a')['href']
             desc = article.find('div', 'sc-1sp7ghq-7').text
-            site.add_article(title, link, desc)
+            img = article.find('img')['data-src']
+
+            site.add_article(title, link, desc, img)
 
         return site.get_dataframe()
 
@@ -176,3 +204,11 @@ if __name__ == '__main__':
     os.system(".\\index.html")
     time.sleep(5)
     os.remove(".\\index.html")
+
+
+# if __name__ == '__main__':
+#     export_to_html(donald())
+#
+#     os.system(".\\index.html")
+#     time.sleep(5)
+#     os.remove(".\\index.html")
